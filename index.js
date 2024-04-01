d7350dec7c1543e7d5f80f58fa780d0b03886f46
vdresume <mrvdparmar96@gmail.com>
2024-04-01 11:25:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+var navLinks = document.getElementById("sidebar");
+    function showMenu(){
+    navLinks.style.left = "0";
+}
+function hideMenu(){
+    navLinks.style.left = "-280px";
+ }
+   
